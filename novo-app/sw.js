90c9b2402626fb61233490112a301699f6ad9a57
leandrofrutal86-a11y <leandrofrutal86@gmail.com>
2026-09-08 17:52:15 -0300

    Adiciona service worker do novo app

---
+++ novo-app/sw.js
@@ -0,0 +1,8 @@
+const CACHE='iturama-v1';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
